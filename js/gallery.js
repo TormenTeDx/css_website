0ae57add5acd67ce8177681717bece63c8abb0fc
Richard <pipilek@gmail.com>
2022-06-19 17:14:23 -0400

    Finish gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-	alert("Hello");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage = document.querySelector("#gallery-photo img");
+
+	let galleryInfo = document.querySelector("#gallery-info");
+	let title = galleryInfo.querySelector(".title");
+	let description = galleryInfo.querySelector(".description");
+
+
+	thumbnails.forEach(function (thumbnail) {
+		thumbnail.addEventListener("click", function (){
+			
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			mainImage.setAttribute("src", newImageSrc);
+			let newImageAlt = thumbnail.dataset.title;
+			mainImage.setAttribute("alt", newImageAlt);
+
+			document.querySelector(".current").classList.remove("current");
+			thumbnail.parentNode.classList.add("current");
+
+			title.innerHTML = thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
+
+		});
+	});
 }
